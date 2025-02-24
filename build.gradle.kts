c3063d7f017544fa3e52c944903157f6828ac383
Ворончихин Иван Романович <bonoberkut@gmail.com>
2025-02-24 15:14:10 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    java
+    jacoco
+    checkstyle
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.jmp"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${property("springdoc-openapi-webmvc-ui.version")}")
+    implementation("org.liquibase:liquibase-core")
+    implementation("org.mapstruct:mapstruct:${property("mapstruct.version")}")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:${property("mapstruct.version")}")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<Checkstyle> {
+    configFile = rootProject.file("checkstyle.xml")
+    maxWarnings = 0
+    isShowViolations = true
+}
+
+tasks.named("checkstyleMain") {
+    dependsOn("compileJava")
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.required = true
+        csv.required = false
+    }
+
+    classDirectories.setFrom(files(classDirectories.files.map {
+        fileTree(it) {
+            include(
+                "**/service/**",
+                "**/controller/**",
+                "**/repository/**"
+            )
+        }
+    }))
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.named("build") {
+    dependsOn("checkstyleMain", "jacocoTestReport")
+}
